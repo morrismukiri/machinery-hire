c20f003cb7f30cf38c96fcd71c6c20c82803a7af
Morris Mukiri <morrismukiri@gmail.com>
2016-07-23 02:59:57 +0300

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,16 +68,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'ec2-54-221-240-149.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd68dm2ss37ml38'),
+            'username' => env('DB_USERNAME', 'ululgbsrkuybex'),
+            'password' => env('DB_PASSWORD', 'wF8gAJ1sDfQmDrMz1xfYwyFSOh'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
         ],
-
     ],
 
     /*
