318902e54875f88bcf2866804e0c6846635369fd
Morris Mukiri <morrismukiri@gmail.com>
2016-08-07 01:41:02 +0300

    fixing https

--- resources/views/layouts/partials/htmlheader.blade.php
+++ resources/views/layouts/partials/htmlheader.blade.php
@@ -1,5 +1,6 @@
 <head>
     <meta charset="UTF-8">
+    <base href="https://machinery-hire.herokuapp.com/" />
     <title>Machinery Hire @yield('htmlheader_title', '') </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.4 -->
